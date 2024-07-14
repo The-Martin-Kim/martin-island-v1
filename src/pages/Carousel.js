febcafc1aef93ed114a26085369c08d2144af264
Martin Kim <iu2407@naver.com>
2024-07-14 15:39:58 +0900

    carousel added

--- src/pages/Carousel.js
+++ src/pages/Carousel.js
@@ -3,6 +3,9 @@ import { CCarousel, CCarouselItem, CImage } from '@coreui/react';
 import img1 from '../img/001.jpg'
 import img2 from '../img/002.jpg'
 import img3 from '../img/003.jpg'
+import img7 from '../img/007.jpg'
+import img8 from '../img/008.jpg'
+import img9 from '../img/009.jpg'
 
 function Carousel_() {
     return (
@@ -17,6 +20,15 @@ function Carousel_() {
           <CCarouselItem>
             <CImage className="d-block w-100 carousel-image" src={img3} alt="slide 3" />
           </CCarouselItem>
+            <CCarouselItem>
+                <CImage className="d-block w-100 carousel-image" src={img7} alt="slide 4" />
+            </CCarouselItem>
+            <CCarouselItem>
+                <CImage className="d-block w-100 carousel-image" src={img8} alt="slide 5" />
+            </CCarouselItem>
+            <CCarouselItem>
+                <CImage className="d-block w-100 carousel-image" src={img9} alt="slide 6" />
+            </CCarouselItem>
         </CCarousel>
       </div>
     );
